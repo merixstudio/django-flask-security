5d9ec25d0c809469d15be2656fe615896ecd2a9f
Wiktor Gonczaronek <w.gonczaronek@merixstudio.com>
2019-01-11 15:28:30 +0100

    CSP - Flask: added security policy

---
+++ CSP/csp-flask/csp.py
@@ -0,0 +1,56 @@
+from flask import make_response
+
+from functools import wraps
+
+__all__ = ('csp',)
+
+
+# Rules for Content Security Policy. This is a dictionary consisting of a key which is directive
+# (like default-src or script-src) and a list or tuple of policies. See https://www.w3.org/TR/CSP2/
+# for further reference. Note that this solution is only intended to serve demonstration purposes,
+# which means it is not suitable for production environment.
+SETTINGS_CSP = {
+
+}
+
+# Same rules for CSP, except for this will only report violations.
+SETTINGS_REPORT_CSP = {
+    'default-src': [
+        '\'self\'',
+    ],
+    'script-src': [
+        '\'self\'',
+        '\'sha256-pu1SeSVRQwuLmgKFdpgMzTi7ghm2F3Ldi/iw1Ff6Myc=\''
+    ]
+}
+REPORT_URI = '/report-csp-violations'
+
+
+def make_csp_header(settings, report_uri=None):
+    header = ''
+    for directive, policies in settings.items():
+        header += f'{directive} '
+        header += ' '.join(
+            (policy for policy in policies)
+        )
+        header += ';'
+    if report_uri:
+        header += f' report-uri {report_uri}'
+    return header
+
+
+def csp(func):
+    @wraps(func)
+    def _csp(*args, **kwargs):
+        response = make_response(func(*args, **kwargs))
+        if SETTINGS_REPORT_CSP:
+            response.headers[
+                'Content-Security-Policy-Report-Only'
+            ] = make_csp_header(SETTINGS_REPORT_CSP, REPORT_URI)
+        if SETTINGS_CSP:
+            response.headers[
+                'Content-Security-Policy'
+            ] = make_csp_header(SETTINGS_CSP)
+        return response
+    return _csp
+
--- CSP/csp-flask/main.py
+++ CSP/csp-flask/main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+
+from csp import csp
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
+@csp
 def home():
     context = {
         'css_class': 'class1',
-        'additional_script': None
+        'additional_script': None,
     }
     if request.method == 'POST':
         if request.form['css_class']:
@@ -16,6 +19,14 @@ def home():
         if request.form['additional_script']:
             context['additional_script'] = request.form['additional_script']
     return render_template('home.html', **context)
+
+
+@app.route('/report-csp-violations', methods=['POST'])
+def report():
+    print(request.data)
+    response = make_response()
+    response.status_code = 200
+    return response
 
 
 if __name__ == '__main__':
